82de924c2d99ee6cced23811e44a54cdf95c7a56
4gboframram <79847791+4gboframram@users.noreply.github.com>
2024-07-24 20:04:49 -0400

    Removed commented out code

--- src/main/java/net/lilyyy411/uwuwumod/UwuwuMod.java
+++ src/main/java/net/lilyyy411/uwuwumod/UwuwuMod.java
@@ -14,30 +14,19 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(UwuwuMod.MOD_ID)
 public class UwuwuMod {
     public static final String MOD_ID = "uwuwu";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    //    public static final DeferredRegister<Item> MOD_ITEMS = DeferredRegister.create(Registries.ITEM, UwuwuMod.MOD_ID);
-//    public static final RegistryObject<Item> ITEM = MOD_ITEMS.register("owo", () -> new Item(new Item.Properties()) {
-//        @Override
-//        public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-//            LOGGER.debug("hover");
-//           pTooltipComponents.add( Component.literal("You're a cute femboy"));
-//        }
-//    });
     public UwuwuMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-//        MOD_ITEMS.register(modEventBus);
+
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
@@ -47,15 +36,4 @@ public class UwuwuMod {
 
     }
 
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-    }
 }
